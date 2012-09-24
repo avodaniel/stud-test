f5e290add368c40fc7ee17c3d4a9505121874b7b
Daniel Fiala <danfiala@ucw.cz>
2012-09-24 23:28:05 +0200

    servers.py : added support for parsing octet with source address at the beginning of the http request.

--- servers.py
+++ servers.py
@@ -3,11 +3,22 @@ from os import curdir, sep
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from optparse import OptionParser
 from multiprocessing import Process
-import os, time
+import os, time, socket
 
 
-def get_handler(id):
+def get_handler(id, header):
     class HagridHandler(BaseHTTPRequestHandler):
+        def handle_one_request(self):
+            self.stud_addr = ""
+            if header:
+                af_type = self.rfile.read(1)
+                if ord(af_type[0]) == socket.AF_INET:
+                    addr_packed = self.rfile.read(4)
+                    addr = socket.inet_ntoa(addr_packed)
+                    self.stud_addr=addr
+            return BaseHTTPRequestHandler.handle_one_request(self)
+
+
         def do_GET(self):
             try:
                 if self.path.endswith(".esp"):   #our dynamic content
@@ -24,6 +35,9 @@ def get_handler(id):
                     self.wfile.write("<p>")
                     self.wfile.write(message)
                     self.wfile.write("</p>")
+                    self.wfile.write("<div><small>")
+                    self.wfile.write("Stud addr: <em>{}</em>".format(self.stud_addr))
+                    self.wfile.write("</small></div>")
                     self.wfile.write("</body></html>")
                     return
                 else:
@@ -33,9 +47,9 @@ def get_handler(id):
     return HagridHandler
 
 
-def create_process(id, port):
+def create_process(id, header, port):
     def fork_main():
-        server = HTTPServer(('', port), get_handler(id))
+        server = HTTPServer(('', port), get_handler(id, header))
         print "Started http server {}, port {}".format(id, port)
         server.serve_forever()
     process = Process(target=fork_main, args=())
@@ -47,11 +61,12 @@ def main():
     parser = OptionParser()
     parser.add_option("-m", "--multi", dest="multi", default="1", metavar="XXXX")
     parser.add_option("-p", "--port", dest="port", default="8080", metavar="XXXX")
+    parser.add_option("-H", "--header", action="store_true", dest="header", metavar="XXXX")
     (options, args) = parser.parse_args()
-
+    print "{!r}".format(options)
     servers = []
     for i in xrange(int(options.multi)):
-        servers.append(create_process(i, int(options.port) + i))
+        servers.append(create_process(i, options.header, int(options.port) + i))
 
     try:
         while True:
